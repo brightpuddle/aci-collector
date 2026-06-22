4e395c2431d268192b2783fd5ea122d7a1fee47b
Nathan Hemingway <nhemingway@gmail.com>
2026-06-22 15:44:52 -0700

    update missing license headers

--- cmd/genscript/main.go
+++ cmd/genscript/main.go
@@ -26,6 +26,25 @@ import (
 	"github.com/brightpuddle/aci-collector/pkg/req"
 )
 
+const ossHeader = `
+
+# SPDX-License-Identifier: Apache-2.0
+
+# Copyright 2026 Cisco Systems, Inc. and their affiliates
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+`
+
 const (
 	tmpFolder = "/tmp/aci-collector"
 )
@@ -56,6 +75,8 @@ func main() {
 
 	// Write script header
 	fmt.Fprintln(f, "#!/bin/bash")
+	fmt.Fprintln(f, "")
+	fmt.Fprintln(f, ossHeader)
 	fmt.Fprintln(f, "")
 	fmt.Fprintf(f, "rm -rf %s > /dev/null\n", tmpFolder)
 	fmt.Fprintf(f, "mkdir %s\n", tmpFolder)
--- test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-pwd = sys.argv[1]
-new_pwd = ""
-
-for a, b in zip(pwd, "_" * len(pwd)):
-    new_pwd += a + b
-
-print(new_pwd)
